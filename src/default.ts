289a5c59a5c07c60bf89b45d2b5fa255b5b47960
zkhyww <127009345+zkhyww@users.noreply.github.com>
2023-04-02 20:22:33 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -8,8 +8,14 @@ export const defaultSetting = {
 
 export const defaultMessage = `
 
-- 特感谢 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。
+- 特感谢 [OpenAI API (gpt-3.5-turbo)]提供支持。本网站无需科学上网，任意环境均可使用。
 
-- 想要部署的朋友，可关注公众号：**凯哥黑科技**，添加我个人微信获取保姆级部署文档。
+- 想要学习的朋友，可关注公众号：**凯哥黑科技**，添加我个人微信获取保姆级学习文档。
+
+- [ChatGPT交流圈](https://mmbiz.qpic.cn/mmbiz_png/P3JJIuzuHrrOHCmnex8iciaNB8THktiakzdAqZoAHgcg3UetxdLyb8bHicLPkL7rBblzkJUXicVahx6ZfibLCbTEtLQg/640?wx_fmt=png&wxfrom=5&wx_lazy=1&wx_co=1)，一起探索ChatGPT以及AI绘画最新玩法。
+
+-- 如需购买独享账号或plus账号可以联系微信[凯哥黑科技](https://https://mmbiz.qpic.cn/mmbiz_png/P3JJIuzuHrrOHCmnex8iciaNB8THktiakzdVg2diagvCO861FoYNbFhTKu95QLlgRnLRy6gNIT2FwVBAPbDxYtAUlw/640?wx_fmt=png&wxfrom=5&wx_lazy=1&wx_co=1)。
 
 - **Shift+Enter** 换行。开头输入 **/** 或者 **空格** 搜索 Prompt 预设。点击输入框滚动到底部。`
+
+
